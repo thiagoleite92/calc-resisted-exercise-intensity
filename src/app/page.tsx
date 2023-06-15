6583aab8132d9627c5a796a6e2ae44e79b853151
Thiago Leite <leite.tjs@gmail.com>
2023-06-15 19:58:54 -0300

    bom comeõ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
-import Image from 'next/image'
+'use client';
+
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { Form } from './components/Form';
+
+const exerciseSchema = z.object({
+  name: z
+    .string()
+    .nonempty('Campo obrigatório')
+    .min(3, 'Mínimo de 3 caracteres')
+    .max(25, 'Máximo de 25 caracteres'),
+  load: z.coerce.number().nonnegative().min(1),
+  reps: z.coerce.number().nonnegative().min(1)
+});
+
+type ExerciseSchemaType = z.infer<typeof exerciseSchema>;
 
 export default function Home() {
+  const exerciseForm = useForm<ExerciseSchemaType>({
+    resolver: zodResolver(exerciseSchema)
+  });
+
+  const { handleSubmit } = exerciseForm;
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className='flex h-full w-full flex-col gap-2  bg-blue-800 py-2'>
+      <FormProvider {...exerciseForm}>
+        <form
+          className='mx-auto grid h-full grid-cols-1 gap-4
+          rounded-sm p-4 sm:mt-4 sm:w-2/4 sm:border sm:border-yellow-400'
+          onSubmit={handleSubmit(onSubmit)}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <h1 className='flex items-center justify-between text-yellow-400'>
+            Predição de Cargas de Treinamento{' '}
+            <button className='h-10 rounded border border-yellow-400 bg-transparent px-3 text-sm font-semibold text-yellow-400 hover:border-black hover:bg-yellow-400 hover:text-black'>
+              {' '}
+              Ver planilha
+            </button>
+          </h1>
+          <div className='flex h-fit flex-col gap-2'>
+            <Form.Field>
+              <Form.Label htmlFor='exercises'>Exercicio</Form.Label>
+              <Form.Input
+                name='exercises'
+                type='text'
+                placeholder='Ex: Deadlift'
+              />
+            </Form.Field>
+            <div className='flex w-full flex-1 justify-between gap-2 sm:justify-start'>
+              <Form.Field>
+                <Form.Label htmlFor='exercises'>Repetições</Form.Label>
+                <Form.Input
+                  name='exercises'
+                  type='number'
+                  min='1'
+                  placeholder='8 reps'
+                />
+              </Form.Field>
+              <Form.Field>
+                <Form.Label htmlFor='exercises'>Carga</Form.Label>
+                <Form.Input
+                  name='exercises'
+                  type='number'
+                  min='1'
+                  placeholder='27kg'
+                />
+              </Form.Field>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <Form.ErrorMessage field='exercises' />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button className='h-10 rounded border border-yellow-400 bg-transparent px-3 text-sm font-semibold text-yellow-400 hover:border-black hover:bg-yellow-400 hover:text-black'>
+            Adicionar à Lista
+          </button>
+        </form>
+      </FormProvider>
+      <button className='text-black-400 h-10 self-center rounded-sm bg-yellow-400 px-3 text-sm font-semibold sm:w-2/4'>
+        Ver Lista
+      </button>
     </main>
-  )
+  );
 }
